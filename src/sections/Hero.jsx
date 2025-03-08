9fa511e3ac991a9e2d97ae0568970b09dc56ead7
Hamid Iqbal <hamid.iqbal00123@gmail.com>
2025-03-08 16:44:40 +0500

    HeroSection completed

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -8,14 +8,18 @@ import { Leva, useControls } from 'leva'
 import { useMediaQuery } from 'react-responsive'
 import { calculateSizes } from '../constants'
 import Target from '../components/Target'
+import ReactLogo from '../components/ReactLogo'
+import Cube from '../components/Cube'
+import Rings from '../components/Rings'
+import HeroCamera from '../components/HeroCamera'
 
 function Hero() {
  
   const isSmall = useMediaQuery({maxWidth:480})
   const isMobile = useMediaQuery({maxWidth:768})
-  const isTalet = useMediaQuery({minWidth:768, maxWidth:1024})
+  const isTablet = useMediaQuery({minWidth:768, maxWidth:1024})
 
-  const sizes = calculateSizes(isSmall,isMobile,isTalet)
+  const sizes = calculateSizes(isSmall,isMobile,isTablet)
   return (
     <section className='min-h-screen w-full flex flex-col relative'>
 
@@ -30,15 +34,21 @@ function Hero() {
           <Canvas className='w-full h-full'>
           <Suspense fallback={<CanvasLoader/>}>
             <PerspectiveCamera makeDefault position={[0,0,20]}/>
+            <HeroCamera isMobile={isMobile}>
+
             <HackerRoom
              rotation={[0,-Math.PI,0]}
              position={sizes.deskPosition}
              scale={sizes.deskScale}
-           
+             
              />
+             </HeroCamera>
 
              <group>
               <Target position={sizes.targetPosition} rotation={[0,Math.PI/5, 0]}/>
+              <ReactLogo position={sizes.reactLogoPosition}/>
+              <Cube position={sizes.cubePosition}/>
+              <Rings position={sizes.ringPosition}/>
              </group>
             <ambientLight intensity={1}/>
             <directionalLight position={[10,10,10]} intensity={0.5}/>
